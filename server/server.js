c2e05fcecca8f8cd1bcfb0a91de0c52d196150cc
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-22 21:40:53 +0530

    added middlewares routes and ports

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import router from "./routes/RegisterApi.js";
+import cors from "cors";
+import connect from "./database/mongodb.js";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log(req.url);
+  res.send("Hello user");
+});
+
+app.use("/api", router)
+
+connect();
+
+app.listen(PORT, () => console.log(`server is running on port ${PORT}...`));
