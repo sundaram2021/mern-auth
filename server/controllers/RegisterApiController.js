8dbda1a171c696568be47ed599edc012d7c78f27
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-23 19:57:55 +0530

    added all the functionality of register and login apis

--- server/controllers/RegisterApiController.js
+++ server/controllers/RegisterApiController.js
@@ -1,5 +1,36 @@
-export const register = async(req, res) => {
-    const { firstName, lastName, email, password } = req.body;
-    res.json({message: "User is created"})
-    console.log(firstName, lastName, email, password);
-}
+import RegisteredUser from "../models/RegisterSchema.js";
+import router from "../routes/RegisterApi.js";
+
+export const register = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  if (!email || !firstName || !lastName || !password) {
+    return res.status(204).json("invalid input");
+  }
+
+  const userExists = await RegisteredUser.findOne({ email });
+  if (userExists) {
+    return res.status(406).json("User already registered");
+  }
+
+  const user = new RegisteredUser({ firstName, lastName, email, password });
+
+  await user.save();
+
+  res.status(201).json({ message: "user is registered" });
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(204).json("invalid input");
+  }
+
+  const user = await RegisteredUser.findOne({ email, password });
+
+  if (!user) {
+    res.status(406).json({ message: "User is not registered" });
+    return;
+  }
+
+  res.json({ message: "User is logged in" });
+};
