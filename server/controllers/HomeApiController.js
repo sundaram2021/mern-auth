e241a923d4b25fd8f39bcd53806976870f3b6c98
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-24 19:50:01 +0530

    modified HomeApiController

--- server/controllers/HomeApiController.js
+++ server/controllers/HomeApiController.js
@@ -4,7 +4,7 @@ import  jwt  from "jsonwebtoken";
 
 export const HomeGet = async(req, res)=> {
     const token = req.headers['x-access-token']
-    const decoded = jwt.verify(token, 'secret');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const email = decoded.email;
     const user = await RegisterSchema.findOne({email})
 
