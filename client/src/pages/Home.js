781270e7c68ef620dbbe708cfb88b389d6bf5275
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-24 19:42:47 +0530

    adding protection to home route

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,42 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import jwt from 'jsonwebtoken'
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import Cookies from "js-cookie";
 
 function Home() {
-  const navigate = useNavigate()
-  const [user, setUser] = useState()
+  const [User, setUser] = useState();
+  const [protect, setProtect] = useState(false)
 
-  async function getUser(){
-    const res = await fetch("http://localhost:8000/api/user", {
-      headers: {
-        'x-access-token': localStorage.getItem('token'),
-      },
-    })
-    
-    const data = await res.json();
-    if(data !== ""){
-      setUser(data.firstName)
-    } else {
-      alert(data.error)
+  async function fetchToken() {
+    try {
+      const token = Cookies.get("token");
+      const res = await fetch("http://localhost:8000/api/login", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      if(token){
+        setProtect(true)
+      }
+
+      const user = await res.json();
+      setUser(user);
+      if (res.ok) {
+        console.log("User data => " + user);
+      }else {
+        throw new Error();
+      }
+    } catch(err) {
+      console.error(err)
     }
   }
 
   useEffect(() => {
-    const token = localStorage.getItem('token')
-    if(token){
-      const user = jwt.decode(token);
-      if(!user){
-        localStorage.removeItem("token")
-        navigate('/login')
-      } else {
-        getUser()
-      }
-    }
-  }, [])
+    fetchToken();
+  });
 
-  return (
-    <h1 style={{textAlign: "center"}}>Hello {user} </h1>
-  )
+  return <h1 style={{ textAlign: "center" }}>Hello {protect && User} </h1>;
 }
 
-export default Home
+export default Home;
