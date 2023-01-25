57f6b6a518ee59326e31b046dc9dde194244fdbf
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-25 22:02:34 +0530

    modified Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,32 +2,37 @@ import React, { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 
 function Home() {
-  const [User, setUser] = useState();
-  const [protect, setProtect] = useState(false)
+  // const [User, setUser] = useState();
+  // const [protect, setProtect] = useState(false);
 
   async function fetchToken() {
     try {
       const token = Cookies.get("token");
+      // console.log(token);
       const res = await fetch("http://localhost:8000/api/login", {
+        method: "POST",
         headers: {
-          Authorization: `Bearer ${token}`,
           "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
         },
       });
 
-      if(token){
-        setProtect(true)
-      }
+      // if (token) {
+      //   setProtect(true);
+      // }
+      // console.log("res => "+res);
 
-      const user = await res.json();
-      setUser(user);
-      if (res.ok) {
+      const contentType = res.headers.get("Content-Type");
+      if (contentType && contentType.indexOf("application/json") !== -1) {
+        console.log("Server is sending a JSON response");
+        const { user } = await res.json();
         console.log("User data => " + user);
-      }else {
-        throw new Error();
+      } else {
+        console.log("Server is not sending a JSON response");
       }
-    } catch(err) {
-      console.error(err)
+    } catch (err) {
+      console.error(err);
     }
   }
 
@@ -35,7 +40,7 @@ function Home() {
     fetchToken();
   });
 
-  return <h1 style={{ textAlign: "center" }}>Hello {protect && User} </h1>;
+  return <h1 style={{ textAlign: "center" }}>Hello</h1>;
 }
 
 export default Home;
