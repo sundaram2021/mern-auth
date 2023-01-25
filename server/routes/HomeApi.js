a3abc34dbe773996730a2c8e7d460dc98d591810
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-25 22:03:28 +0530

    added HomeApi routes into function

--- server/routes/HomeApi.js
+++ server/routes/HomeApi.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { HomeGet } from '../controllers/HomeApiController.js'
-const router = Router();
+const router2 = Router();
 
-router.get('/user', HomeGet)
-// router.post('/user', HomePost)
+router2.get('/', HomeGet)
+
+export default router2;
