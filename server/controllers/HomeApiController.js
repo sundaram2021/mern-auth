de64f8bef8ac92a461d956bfc379b62610cf56de
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-25 22:03:56 +0530

    modified controllers

--- server/controllers/HomeApiController.js
+++ server/controllers/HomeApiController.js
@@ -1,12 +1,31 @@
-import RegisterSchema from "../models/RegisterSchema";
-import  jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
+export const HomeGet = async (req, res) => {
+//   // const token = req.headers['x-access-token'];
+//   const authHeader = req.headers.authorization;
+// //   if (authHeader) {
+// //     const token = authHeader.split(" ")[1];
+// //     // validate and use the token
+// //   }
 
-export const HomeGet = async(req, res)=> {
-    const token = req.headers['x-access-token']
+  authHeader ? ( token = authHeader.split(" ")[1]): " ";
+
+//   const token =
+//     req.body.token || req.headers["x-access-token"] || req.query.token;
+//   // if (!token) {
+//   //     return res.status(401).json({ message: 'No token provided' });
+//   // }
+
+//   const token = jwt.get("token");
+
+  try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const email = decoded.email;
-    const user = await RegisterSchema.findOne({email})
+    req.user = decoded.user;
 
-    return res.json({user})
-}
+    const { user } = req.user;
+    return res.json({ message: "User is created", user });
+    // next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
--- server/controllers/RegisterApiController.js
+++ server/controllers/RegisterApiController.js
@@ -1,8 +1,8 @@
 import RegisteredUser from "../models/RegisterSchema.js";
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-export const register = async(req, res) => {
+export const register = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
   if (!email || !firstName || !lastName || !password) {
     return res.status(204).json("invalid input");
@@ -17,7 +17,12 @@ export const register = async(req, res) => {
   const salt = await bcrypt.genSalt(saltRounds);
   const hashedPassword = await bcrypt.hash(password, salt); // 10 -> saltrounds
 
-  const user = new RegisteredUser({ firstName, lastName, email, password: hashedPassword });
+  const user = new RegisteredUser({
+    firstName,
+    lastName,
+    email,
+    password: hashedPassword,
+  });
 
   await user.save();
 
@@ -29,26 +34,30 @@ export const login = async (req, res) => {
   if (!email || !password) {
     return res.status(204).json("invalid input");
   }
+  try {
+    const user = await RegisteredUser.findOne({ email });
 
-  const user = await RegisteredUser.findOne({ email });
+    if (!user) {
+      return res.status(406).json({ message: "User is not registered" });
+    }
 
-  if (!user) {
-    res.status(406).json({ message: "User is not registered" });
-    return;
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      return res.status(401).json({ message: "Password doesn't matched" });
+    }
+
+    const payload = {
+      email: user.email,
+      id: user._id,
+    };
+    console.log("user...");
+    const token = jwt.sign(payload, process.env.JWT_SECRET);
+    // console.log(window.location.href);
+    return res
+      .status(201)
+      .json({ message: "Successfully logged in", token, user });
+  } catch (err) {
+    return res.status(408).json({ message: "server error" });
   }
-
-  const match = await bcrypt.compare(password, user.password);
-
-  if(!match){
-    res.status(401).json({ message: "Password doesn't matched" })
-    return
-  }
-
-  const payload = {
-    email: user.email,
-    id: user._id
-  }
-
-  const token = jwt.sign(payload, process.env.JWT_SECRET) ;
-  return res.json({ message: "Successfully logged in", token, user })
 };
