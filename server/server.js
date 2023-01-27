4f1a7d88641e22a652c7632fd4869bd0b3aa1c90
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-27 18:57:45 +0530

    modified

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 import express from "express";
 import * as dotenv from "dotenv";
+dotenv.config();
+import cookieParser from "cookie-parser";
 import router from "./routes/RegisterApi.js";
 import cors from "cors";
 import connect from "./database/mongodb.js";
 import router2 from "./routes/HomeApi.js";
-dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT;
+app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
 
@@ -15,6 +18,8 @@ app.use(express.json());
 
 app.use("/api", router);
 app.use('/', router2);
+
+
 
 connect();
 
