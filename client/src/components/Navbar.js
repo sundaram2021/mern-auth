85d03d25f75a5a797a1e686a6efebfd90e780b09
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-28 15:33:34 +0530

    added logout functionality

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,23 +1,45 @@
-import * as React from "react";
+import React, {  } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import { Link } from "react-router-dom";
-import '../App.css'
+import { Link, useNavigate } from "react-router-dom";
+import "../App.css";
 
 export default function ButtonAppBar() {
+  const navigate = useNavigate();
+  // const [myToken, setMyToken] = useState(false);
+
+  // const token = localStorage.getItem("token");
+
+  // if (token) {
+  //   setMyToken(true);
+  // }
+
+  async function removeToken() {
+    localStorage.removeItem("token");
+    navigate("/login");
+  }
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
-            <Link to='/' className="white">Mern Auth</Link>
+            <Link to="/" className="white">
+              Mern Auth
+            </Link>
           </Typography>
-          <Link to='/login' className="white"><Button color="inherit">Login</Button></Link>
-          <Button color="inherit">Logout</Button>
-          <Link to="/register" className="white"><Button color="inherit">Register</Button></Link>
+          <Button color="inherit" onClick={removeToken}>
+            Logout
+          </Button>
+          <Link to="/login" className="white">
+            <Button color="inherit">Login</Button>
+          </Link>
+          <Link to="/register" className="white">
+            <Button color="inherit">Register</Button>
+          </Link>
         </Toolbar>
       </AppBar>
     </Box>
