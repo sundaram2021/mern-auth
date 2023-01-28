04a01acefaab91111bee2237f25dbe6fd7e97a45
Sundaram Kumar Jha <jhasundarm@gmail.com>
2023-01-28 15:07:15 +0530

    modified

--- server/server.js
+++ server/server.js
@@ -7,19 +7,26 @@ import cors from "cors";
 import connect from "./database/mongodb.js";
 import router2 from "./routes/HomeApi.js";
 
-
 const app = express();
 const PORT = process.env.PORT;
 app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
-
-
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 app.use("/api", router);
-app.use('/', router2);
-
-
+app.use("/", router2);
 
 connect();
 
